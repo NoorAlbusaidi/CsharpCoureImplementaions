bff0bfcbdf8f492e4960f9f23fea8dc11a1cdb61
NoorAlbusaidi <nooralhuda.albusaidi@gmail.com>
2026-06-02 15:21:52 +0400

    Lists Practice Task Sheet project

---
+++ listsPracticeTaskSheet/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace listsPracticeTaskSheet
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
