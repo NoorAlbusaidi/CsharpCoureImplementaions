42ad8b01650180981c01a4b3cf835e55e0580573
NoorAlbusaidi <nooralhuda.albusaidi@gmail.com>
2026-06-07 12:20:13 +0400

    done with case 03 ( Book a Flight Ticket)

--- miniFlightManagementSystem/Program.cs
+++ miniFlightManagementSystem/Program.cs
@@ -7,18 +7,28 @@
         static List<string> ticketNumbers = new List<string>{"T001","T002","T003","T004","T005"};
 
         static List<string> cancelledTickets = new List<string>();
+
+        //dd-MMM-yyyy
+        static List<DateTime> availableDates = new List<DateTime> {
+        new DateTime(2026, 1, 1),
+        new DateTime(2026, 1, 5),
+        new DateTime(2026, 1, 10),
+        new DateTime(2026, 1, 15)
+        };
+        static Dictionary<string, string> bookingRecord = new Dictionary<string, string>();
+        static string[] flightNumbers = new string[] { "OA101", "OA102", "OA103", "OA104", "OA105", "OA106" };
         public static void registerNewPassenger() {
             bool exist = false;
             Console.Write("Enter passenger's full name: ");
             string fullName = Console.ReadLine();
-            fullName = fullName.Trim();
+            
             // Validation
             if (string.IsNullOrWhiteSpace(fullName))
             {
                 Console.WriteLine("Error: Name cannot be empty.");
                 return; // go back to menu
             }
-
+            fullName = fullName.Trim();
             for (int i = 0; i < passengerNames.Count; i++) {
                 
                 if (fullName.ToLower() == passengerNames[i].ToLower()) {
@@ -84,6 +94,98 @@
             Console.WriteLine("The Total passengers: "+passengerNames.Count);
 
         }
+
+        public static void bookFlightTicket() {
+            Console.Write("Enter Ticket ID: ");
+            string ticketID = Console.ReadLine();
+            string bookingValue;
+            DateTime selectedDate;
+            string selectedFlight;
+
+            if (string.IsNullOrWhiteSpace(ticketID))
+            {
+                Console.WriteLine("Error: Ticket ID cannot be empty.");
+                return;
+            }
+
+            ticketID = ticketID.Trim();
+
+            // Check if ticket exists
+            if (!ticketNumbers.Contains(ticketID))
+            {
+                Console.WriteLine("Error: Ticket ID does not exist.");
+                return;
+            }
+
+            // Check if ticket is cancelled
+            if (cancelledTickets.Contains(ticketID))
+            {
+                Console.WriteLine("Error: Ticket is already cancelled.");
+                return;
+            }
+
+            // If valid
+            Console.WriteLine("Ticket is valid.");
+
+            // Check if ticket already has a booking
+            if (bookingRecord.ContainsKey(ticketID))
+            {
+                Console.WriteLine("Error: This ticket already has a booking.");
+                return;
+            }
+            if (flightNumbers.Length == 0)
+            {
+                Console.WriteLine("No flights available.");
+                return;
+            }
+            Console.WriteLine("\nAvailable Flights:");
+
+            for (int i = 0; i < flightNumbers.Length; i++)
+            {
+                Console.WriteLine($"{i + 1}. {flightNumbers[i]}");
+            }
+
+            // Prompt user
+            Console.Write("Select a flight (enter number 1-6): ");
+            int flightChoice;
+            flightChoice = int.Parse(Console.ReadLine());
+            if (flightChoice < 1 || flightChoice > 6) {
+                Console.WriteLine("Error: Selection out of range.");
+                return;
+            }
+
+            // Display dates
+            Console.WriteLine("Available Dates:");
+            for (int i = 0; i < availableDates.Count; i++)
+            {
+                Console.WriteLine($"{i + 1}. {availableDates[i].ToString("dd-MMM-yyyy")}");
+            }
+            Console.Write("Select a date by entering its number: ");
+            int dateChoice;
+            dateChoice = int.Parse(Console.ReadLine());
+
+            if (dateChoice < 1 || dateChoice > 4)
+            {
+                Console.WriteLine("Error: Selection out of range.");
+                return;
+            }
+            //Get selected flight
+            selectedFlight = flightNumbers[flightChoice - 1];
+            // Get selected date
+            selectedDate = availableDates[dateChoice - 1];
+            //a value of the dictionary
+            bookingValue = selectedFlight + "|" + selectedDate;
+
+            // Store in dictionary
+            bookingRecord.Add(ticketID, bookingValue);
+
+            string passName = passengerNames[ticketNumbers.IndexOf(ticketID)];
+            //showing details
+            Console.WriteLine("\nBooking successful!");
+            Console.WriteLine("Details: "+ ticketID+ " of "+ passName+ ": "  + bookingValue);
+        }
+
+
         static void Main(string[] args)
         {
             int choice;
@@ -118,7 +220,7 @@
                         break;
 
                     case 3:
-                        
+                        bookFlightTicket();
                         break;
 
                     case 4:
